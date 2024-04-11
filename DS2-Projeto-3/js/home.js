8d032904ce84e47df8be666c0d9e18daba8bf3fb
LUIZHVA <luizhva02@gmail.com>
2024-04-11 13:58:50 -0300

    13:58-11/04/2024

--- DS2-Projeto-3/js/home.js
+++ DS2-Projeto-3/js/home.js
@@ -2,12 +2,14 @@
 
 const body = document.getElementById("body")
 const mudarTema = document.getElementById("mudarTema")
-const main_conta = document.getElementById("main-container")
+const main_container = document.getElementById("main-container")
 const texto = document.getElementById("inputFraseOriginal")
 const traducao = document.getElementById("traducao")
 const creditoGUI = document.getElementById("creditoGUI")
 const creditoVidal = document.getElementById("creditoVidal")
 
+const microfone_in = document.getElementById("microfone_in");
+const alto_falante_out = document.getElementById("alto_falante_out")
 const botao = document.getElementById("botao")
 const selects = document.querySelectorAll("select")
 const selectTo = document.getElementById("selectTo")
@@ -41,25 +43,29 @@ selects.forEach((select) => {
 function mudarTemaDarkPage() {
     body.classList.toggle('bodyDark')
     mudarTema.classList.toggle('sol')
-    main_conta.classList.toggle('dark-main-container')
+    main_container.classList.toggle('dark-main-container')
     texto.classList.toggle('inputFraseOriginalDark')
     traducao.classList.toggle('traducaoDark')
+    microfone_in.classList.toggle('microfone_in_dark')
+    alto_falante_out.classList.toggle('alto_falante_out_dark')
     botao.classList.toggle('botaoDark')
     creditoGUI.classList.toggle('creditoGuiDark')
     creditoVidal.classList.toggle('creditoVidalDark')
 }
 
 function traducaoAPI() {
+    if (texto.value.toLowerCase() == 'alice') {
+        mudarTemaDarkPage()
+    }
+    
     fetch(
         `https://api.mymemory.translated.net/get?q=${texto.value}&langpair=${selects[0].value}|${selects[1].value}`
-    )
-        .then((res) => res.json())
+    )    
+    .then((res) => res.json())
         .then((data) => {
             traducao.value = data.responseData.translatedText;
         });
-    if (traducao.value.toLowerCase() == 'alice') {
-        mudarTemaDarkPage()
-    }
+    
 }
 
 mudarTema.addEventListener('click', mudarTemaDarkPage)
@@ -72,4 +78,27 @@ botao.addEventListener("click", () => {
     }
 });
 
+window.addEventListener('DOMContentLoaded', function(e){
+
+    if(window.SpeechRecognition || window.
+        webkitSpeechRecognition) {
+
+            var SpeechRecognition = SpeechRecognition ||
+            webkitSpeechRecognition;
+
+            var recognition = new SpeechRecognition();
+
+            microfone_in.addEventListener('click', function(e){
+                recognition.start();
+
+                recognition.addEventListener('result', (event) =>{
+                    texto.value = event.results[0][0].transcript
+                    traducaoAPI()
+                })
+
+            })
+    }else{
+        alert('O navegador não suporta esta funcionalidade!!');
+    }
+},false);
 
